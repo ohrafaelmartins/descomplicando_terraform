5c2d3ae678711b41f7209ff245aab8a78c437820
rafael.martins <rafaelmartins025@outlook.com>
2021-03-14 14:22:55 -0300

    using data aws_ami

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,16 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Ubuntu
+}
+
 resource "aws_instance" "web" {
-  ami           = "ami-0b9517e2052e8be7a"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
 
   tags = {
