da49bc2d60650880bb52de7869f5fc400a493349
rafael martins <rafaelmartins05@outlook.com>
2021-03-21 19:32:50 -0300

    key_name

--- ec2.tf
+++ ec2.tf
@@ -10,10 +10,14 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.micro"
+  count                       = 5
+  associate_public_ip_address = true
+  key_name                    = "descomplicando"
+
 
   tags = {
-    Name = "HelloWorld"
+    Name = var.instance_names[count.index]
   }
 }
